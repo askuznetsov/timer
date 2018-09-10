74582466103901f4e1bd1102c5e05da4182a980a
Oleksandr KUZNETSOV <okuznets@e1r12p2.unit.ua>
2018-09-10 15:35:05 +0300

    ....timer. version 0.2

--- timer.c
+++ timer.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
 
 int 	minutes(int min)
@@ -8,52 +6,6 @@ int 	minutes(int min)
 
 	sec = min * 60;
 	return (sec);
-}
-
-int 	ft_is_delimeter(char temp)
-{
-	if (temp == ' ' || temp == '\n' || temp == '\t' ||
-		temp == '\v' || temp == '\r' || temp == '\f')
-		return (1);
-	return (0);
-}
-
-int 	ft_isdigit(int c)
-{
-	if (c >= 48 && c <= 57)
-		return (1);
-	else
-		return (0);
-}
-
-int		ft_atoi(char *str)
-{
-		unsigned long int	number;
-		int					i;
-		int 				minus;
-
-		i = 0;
-		minus = 1;
-		number = 0;
-		while (ft_is_delimeter(str[i]))
-			i++;
-		if (str[i] == '-')
-			minus = -1;
-		if (str[i] == '-' || str[i] == '+')
-			i++;
-		while (str[i] && ft_isdigit(str[i]))
-		{
-			if ((number > 922337203685477580 ||
-				(number == 922337203685477580 &&
-				(str[i] - '0') > 7)) && minus == 1)
-				return (-1);
-			else if ((number > 922337203685477580 ||
-				(number == 922337203685477580 &&
-				(str[i] - '0') > 8)) && minus == -1)
-				return (0);
-			number = (number * 10) + (str[i++] - '0');
-		}
-		return (number * minus);
 }
 
 int		main(void)
@@ -90,17 +42,21 @@ int		main(void)
 		int timer;
 		int hrs;
 		int minutes;
-		int er;
+		int error;
 
 		printf("Type number of hours:\n");
-		if (er = scanf("%d", hrs) != 1)
+		if (error = scanf("%d", hrs) != 1)
 			printf("Error input\n");
 		else if (hrs < 0)
 			printf("Error input"\n);
 		else 
 		printf("Type number of minutes:\n");
-		if (er = scanf("%d", minutes) != 1)
+		if (error = scanf("%d", minutes) != 1)
 			printf("Error input\n");
-		timer = 0;
-		while (timer
+		else if (minutes < 0 || minutes >= 60)
+			printf("Error input\n");
+		else
+		{
+			timer = 0;
+			while (timer < 
 }
